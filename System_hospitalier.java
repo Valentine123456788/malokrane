310ca4285c60514b0920fb3ae5fb17709ce264d5
“SANDRIN <valentine.sandrin15@gmail.com>
2024-02-22 10:14:30 +0100

    Nouvelle version du fichier

--- System_hospitalier.java
+++ System_hospitalier.java
@@ -34,12 +34,12 @@ public class System_hospitalier {
     }
 
     private String analyserRequete(Requête requête) {
-        
+        // Analyses des requêtes...
         return "requête analysée";
     }
 
     private String comparerDonnes() {
-        
+        // Comparaison des données
         return "Cluster trouvé";
     }
 
